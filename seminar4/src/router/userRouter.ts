1e458c46ae66fd13d8d366dca6236f5d031c66b5
hannah100-coder <pd206@naver.com>
2022-11-25 15:37:28 +0900

    seminar06_basic_seminarCode

--- seminar4/src/router/userRouter.ts
+++ seminar4/src/router/userRouter.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { userController } from "../controller";
+import { auth } from "../middlewares";
+import { body } from "express-validator"
 
 const router: Router = Router();
 
-router.get("/:userId", userController.getUserById);
+//router.get("/:userId", userController.getUserById);
+router.get("/:userId", auth, userController.getUserById);
 
 //* 유저 생성 - POST api/user
-router.post('/', userController.createUser);
+//router.post('/', userController.createUser);
 
 //* 전체 유저 조회 - GET api/user
 router.get('/', userController.getAllUser);
@@ -16,5 +19,27 @@ router.patch('/:userId', userController.updateUser);
 
 //* 유저 삭제 - DELETE api/user/:userId
 router.delete('/:userId', userController.deleteUser);
+
+
+//==== Seminar6
+//* 유저 생성(회원가입) - POST api/user
+router.post(
+    "/",
+    [body("name").notEmpty(), body("email").notEmpty(), body("password").isLength({ min: 6 })],
+    userController.createUser
+  );
+  //중간에 들어가는 값: middleware을 배열로 보내주는 것임
+
+//* 로그인 - POST api/user/signin
+router.post(
+  "/signin",
+  [
+    body("email").notEmpty(),
+    body("email").isEmail(),
+    body("password").notEmpty(),
+    body("password").isLength({ min: 6 }),
+  ],
+  userController.signInUser
+);
 
 export default router;
--- seminar4/src/service/userService.ts
+++ seminar4/src/service/userService.ts
@@ -1,19 +1,43 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import { sc } from "../constants";
+import { UserCreateDTO } from "../interfaces/UserCreateDTO";
+import { UserSignInDTO } from "../interfaces/userSignInDTO";
+
 const prisma = new PrismaClient();
 
 //* 유저 생성
-const createUser = async (name: string, email: string, age: number) => {
+// const createUser = async (name: string, email: string, age: number) => {
 
-  const data = await prisma.user.create({
+//   const data = await prisma.user.create({
+//     data: {
+//       userName: name,  //name 이라는 이름으로 받은 값을 userName이라는 필드에 담는다
+//       age,
+//       email
+//     },
+//   });
+// return data;
+// };
+
+//* 유저 생성
+const createUser = async (userCreateDto: UserCreateDTO) => {
+  //? 넘겨받은 password를 bcrypt의 도움을 받아 암호화
+  const salt = await bcrypt.genSalt(10); //^ 매우 작은 임의의 랜덤 텍스트 salt. genSalt: generateSalt. (N): N길이의 텍스트 생성하겠따
+  const password = await bcrypt.hash(userCreateDto.password, salt); //^ 위에서 랜덤을 생성한 salt를 이용해 암호화
+
+  const data = await prisma.user_seminar6.create({
     data: {
-      userName: name,  //name 이라는 이름으로 받은 값을 userName이라는 필드에 담는다
-      age,
-      email
+      userName: userCreateDto?.name,
+      age: userCreateDto?.age,
+      email: userCreateDto.email,
+      password,
     },
   });
 
   return data;
 };
+
+
 
 //* 유저 전체 조회
 const getAllUser = async () => {
@@ -55,12 +79,36 @@ const getUserById = async (userId: number) => {
   return user;
 };
 
+//* 로그인
+const signIn = async (userSignInDto: UserSignInDTO) => {
+  try {
+    const user = await prisma.user_seminar6.findFirst({
+      where: {
+        email: userSignInDto.email,
+      },
+    });
+    if (!user) return null;
+
+    //? bcrypt가 DB에 저장된 기존 password와 넘겨 받은 password를 대조하고,
+    //? match false시 401을 리턴
+    const isMatch = await bcrypt.compare(userSignInDto.password, user.password);
+    if (!isMatch) return sc.UNAUTHORIZED;
+
+    return user.id;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+
 const userService = {
   createUser,
   getAllUser,
   updateUser,
   deleteUser,
   getUserById,
+  signIn,
 };
 
 export default userService;
